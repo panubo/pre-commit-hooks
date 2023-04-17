2291d652e6a4fc39ac263199d3a558847c25186a
Tim Robinson <tim@panubo.com>
2023-04-17 17:12:44 +1000

    Add v0.0.1

---
+++ README.md
@@ -0,0 +1,6 @@
+# Pre-Commit Hooks
+
+This repo contains configs and plugins for the [pre-commit](https://pre-commit.com/)
+framework.
+
+- `bin` - the plugins / pre-commit hooks.
---
+++ bin/ansible-check-encrypted-secrets.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+is_encrypted() {
+  local file=$1
+  if ! git show :"$file" | grep --quiet "^\$ANSIBLE_VAULT"; then
+    echo "Unencrypted file detected!"
+    exit 1
+  fi
+}
+
+echo "Running pre-commit checks..."
+for FILE in "${@}"; do
+  git diff --cached --name-only | grep "${FILE}" | while IFS= read -r line; do
+    is_encrypted "${line}"
+  done
+done
