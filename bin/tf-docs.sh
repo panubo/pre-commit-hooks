1f5fbdc43f2e073184302eb75f8d56386fb23c71
Andrew Cutler <andrew@panubo.com>
2025-08-28 10:57:53 +1000

    Working now

---
+++ bin/tf-docs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# LICENSE: The MIT License Copyright (c) 2025 Volt Grid Pty Ltd t/a Panubo
+
+# Runs Terraform Docs
+
+set -e
+
+if ! command -v terraform-docs >/dev/null 2>&1; then
+  echo "Error: terraform-docs not found"
+  exit 1
+fi
+
+echo "Running $(basename "${0}") pre-commit checks..."
+for FILE in "${@}"; do
+  terraform-docs fmt "${FILE}"
+done
