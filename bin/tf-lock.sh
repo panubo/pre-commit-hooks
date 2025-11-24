5b3036f453429b58a09ab366fe2f6e82e276dd8e
Tim Robinson <tim@panubo.com>
2025-11-24 15:03:45 +1100

    Add tf-lock hook

---
+++ bin/tf-lock.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# LICENSE: The MIT License Copyright (c) 2025 Volt Grid Pty Ltd t/a Panubo
+
+# Runs Terraform Lint
+
+set -e
+
+if ! command -v tofu >/dev/null 2>&1; then
+  echo "Error: tofu not found"
+  exit 1
+fi
+
+echo "Running $(basename "${0}") pre-commit checks..."
+for FILE in "${@}"; do
+  pushd "$(basedir "${FILE}")"
+  tofu providers lock -platform=linux_amd64
+  popd
+done
